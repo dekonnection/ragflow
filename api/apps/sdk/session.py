09002b5b9c7cd34f30f7dff3dfa191db6444033e
Cédric Laudrel <cedric.laudrel@iguanesolutions.com>
2025-03-20 16:08:30 +0100

    add debug

--- api/apps/sdk/session.py
+++ api/apps/sdk/session.py
@@ -253,6 +253,7 @@ def chat_completion_openai_like(tenant_id, chat_id):
     req = request.json
 
     messages = req.get("messages", [])
+    print(f"Messages: {messages}")
     # To prevent empty [] input
     if len(messages) < 1:
         return get_error_data_result("You have to provide messages.")
@@ -274,6 +275,7 @@ def chat_completion_openai_like(tenant_id, chat_id):
 
     # Filter system and non-sense assistant messages
     msg = [m for m in messages if m["role"] != "system"][::-5]
+    print(f"MSG: {msg}")
 
     if req.get("stream", True):
         # The value for the usage field on all chunks except for the last one will be null.
